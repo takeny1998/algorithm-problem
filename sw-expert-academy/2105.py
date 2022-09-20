bf8632818e27cab5acf03fb2e6123801d4201fac
Heebeom Yang <takeny1998@gmail.com>
2022-09-20 17:57:09 +0900

    Update 2105.py

--- sw-expert-academy/2105.py
+++ sw-expert-academy/2105.py
@@ -1,53 +1,41 @@
-from tkinter import N
-
-
-move = [(1, 1), (1, -1), (-1, -1), (-1, 1)]
-
-def solution(size: int):
-    global iy, ix, answer
-    field: list[list] = []
-    answer = -1
-
-    for _ in range(size):
-        field.append(list(map(int, input().split())))
+def dfs(y, x, d, visited):
+    global iy, ix, answer, move
+    # end조건
+    if (y, x) == (iy, ix) and len(visited) > 1 and d == 3:
+        answer = max(answer, len(visited))
+        return
     
-    def dfs(y: int, x: int, visited: list[tuple], meet: list):
-        global iy, ix, answer
-        
-        for d in range(4):
-            next_y: int = y + move[d][0]
-            next_x: int = x + move[d][1]
+    if (0 <= y < size) and (0 <= x < size) and (field[y][x] not in visited):
+        visited.append(field[y][x])
 
-            # 범위검사
-            if (0 <= next_y < size) and (0 <= next_x < size):
-                next_menu: int = field[next_y][next_x]
-                
-                # 끝나기 검사
-                if len(visited) > 1:
-                    end_y, end_x = visited[0][0] + (visited[1][0] * -1), visited[0][1] + (visited[1][1] * -1)
-                    
-                    if (end_y, end_x) == (next_y, next_x):
-                        answer = max(answer, len(visited))
-                        return
-                # 방문검사
-                if (next_y, next_x) not in visited:
-                    if next_menu not in meet:
-                        visited.append((next_y, next_x))
-                        meet.append(next_menu)
-                        dfs(next_y, next_x, visited[:], meet[:])
-                        meet.pop()
-                        visited.pop()
-    
-    for y in range(size):
-        for x in range(size):
-            dfs(y, x, [(y, x)], [field[y][x]])
+        # 직진할 때
+        ny, nx = y + move[d][0], x + move[d][1]
+        dfs(ny, nx, d, visited[:])
 
-    print(answer)
+        # 회전할 때
+        if d < 3:
+            ny, nx = y + move[d + 1][0], x + move[d + 1][1]
+            dfs(ny, nx, d + 1, visited[:])
+
 
 import sys
 sys.stdin = open("input.txt", "r")
 
 T = int(input())
+
 # 여러개의 테스트 케이스가 주어지므로, 각각을 처리합니다.
 for test_case in range(1, T + 1):
-    solution(int(input()))
+    move = [(1, 1), (1, -1), (-1, -1), (-1, 1)]
+    answer = -1
+
+    size = int(input())
+    field = []
+    
+    for _ in range(size):
+        field.append(list(map(int, input().split())))
+
+    for iy in range(size):
+        for ix in range(size):
+            dfs(iy, ix, 0, list())
+
+    print(f'#{test_case} {answer}')
