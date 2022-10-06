a34a40a596e479c4d24d78181b3b110e175d3525
Heebeom Yang <takeny1998@gmail.com>
2022-10-06 18:04:08 +0900

    Update p14888.py

--- baekjoon/p14888.py
+++ baekjoon/p14888.py
@@ -1,8 +1,50 @@
 def solution():
+    answer = []
+
+    # dfs 순회
+    def dfs(idx, opers, oper_num):
+        if idx == (N - 1):
+            answer.append(cal(opers))
+            return opers
+        
+        # 연산자별로 끼우기
+        for o in range(4):
+            if oper_num[o] > 0:
+                oper_num[o] -= 1
+                opers[idx] = o
+                dfs(idx + 1, opers[:], oper_num[:])
+                oper_num[o] += 1
+                opers[idx] = -1
+
+
+    # 항 계산하기
+    def cal(opers):
+        result = nums[0]
+        idx = 1
+        for oper in opers:
+            if oper == 0: # 덧셈
+                result += nums[idx]
+            elif oper == 1: # 뺄셈
+                result -= nums[idx]
+            elif oper == 2: # 곱셈
+                result *= nums[idx]
+            elif oper == 3: # 나눗셈
+                result /= nums[idx]
+                result = int(result)
+
+            idx += 1
+
+        return result
+
+
+    dfs(0, [-1 for _ in range(N - 1)], oper_num)
+
+    print(max(answer))
+    print(min(answer))
     return
+
 
 N = int(input())
 nums = list(map(int, input().split()))
-operator_num = list(map(int, input().split()))
-
-print(nums, operator_num)
+oper_num = list(map(int, input().split()))
+solution()
