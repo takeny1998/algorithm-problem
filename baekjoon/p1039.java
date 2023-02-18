2f6e03d12afcf86cb9e6e627a8042f18c86b2a6f
Heebeom Yang <takeny1998@gmail.com>
2023-02-18 22:30:54 +0900

    add Problems

--- baekjoon/p1039.java
+++ baekjoon/p1039.java
@@ -1,32 +1,67 @@
-import java.util.Arrays;
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class p1039 {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
 
-        final int N = sc.nextInt();
+    static int N, M, K;
+    static int[][] dp;
 
-        int[] input = new int[3], temp = new int[3],
-                min = new int[3], max = new int[3];
+    static String swap(String num, int a, int b) {
+        StringBuilder sb = new StringBuilder(num);
 
-        for (int i = 0; i < N; i ++) {
-            for (int r = 0; r < 3; r ++)
-                input[r] = sc.nextInt();;
+        char temp = sb.charAt(a);
+        sb.setCharAt(a, sb.charAt(b));
+        sb.setCharAt(b, temp);
 
-            temp[0] = Math.min(min[0], min[1]) + input[0];
-            temp[1] = Math.min(min[0], Math.min(min[1], min[2])) + input[1];
-            temp[2] = Math.min(min[1], min[2]) + input[2];
-            System.arraycopy(temp, 0, min, 0, 3);
+        return sb.toString();
+    }
 
-            temp[0] = Math.max(max[0], max[1]) + input[0];
-            temp[1] = Math.max(max[0], Math.max(max[1], max[2])) + input[1];
-            temp[2] = Math.max(max[1], max[2]) + input[2];
-            System.arraycopy(temp, 0, max, 0, 3);
+    static int recursion(int depth, String numStr) {
+        int num = Integer.parseInt(numStr);
+        if (depth == K) {
+            return num;
         }
 
-        System.out.printf("%d %d",
-                Math.max(max[0], Math.max(max[1], max[2])),
-                Math.min(min[0], Math.min(min[1], min[2])));
+        int ret = dp[depth][num];
+
+        if (ret != 0) {
+            return ret;
+        }
+
+        ret = -1;
+        for (int i = 0; i < M - 1; i ++) {
+            for (int j = i + 1; j < M; j ++) {
+                String swapStr = swap(numStr, i, j);
+                if (swapStr.charAt(0) == '0') {
+                    continue;
+                }
+
+                int nextRet = recursion(depth + 1, swapStr);
+                if (nextRet > ret) {
+                    ret = nextRet;
+                }
+            }
+        }
+
+        dp[depth][num] = ret;
+        return ret;
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        K = Integer.parseInt(st.nextToken());
+        M = String.valueOf(N).split("").length;
+
+        dp = new int[K][1000001];
+
+        int answer = -1;
+        answer = recursion(0,String.valueOf(N));
+
+        System.out.println(answer);
     }
 }
