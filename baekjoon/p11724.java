2dd04f2a974a83afb0cb8f32a2b52eae73406c97
takeny1998 <89298198+takeny1998@users.noreply.github.com>
2024-02-04 23:06:04 +0900

    Solve p1181, p2839, p11724, p13549

---
+++ baekjoon/p11724.java
@@ -0,0 +1,60 @@
+import java.io.*;
+import java.util.*;
+
+public class p11724 {
+    private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    private static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    private static StringTokenizer st;
+    private static boolean[] isVisited;
+    private static ArrayList<Integer>[] graphs;
+
+    private static void bfs(int start) {
+        Queue<Integer> queue = new LinkedList<>();
+        isVisited[start] = true;
+
+        queue.add(start);
+
+        while (!queue.isEmpty()) {
+            int curt = queue.poll();
+
+            for (int next : graphs[curt]) {
+                if (isVisited[next]) continue;
+                isVisited[next] = true;
+                queue.add(next);
+            }
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        isVisited = new boolean[N + 1];
+        graphs = new ArrayList[N + 1];
+
+        for (int i = 0; i <= N; i ++) {
+            graphs[i] = new ArrayList<>();
+        }
+
+        for (int i = 0; i < M; i ++) {
+            st = new StringTokenizer(br.readLine());
+            int u = Integer.parseInt(st.nextToken());
+            int v = Integer.parseInt(st.nextToken());
+
+            graphs[u].add(v);
+            graphs[v].add(u);
+        }
+        int result = 0;
+
+        for (int i = 1; i <= N; i ++) {
+            if (isVisited[i]) continue;
+            bfs(i);
+            result ++;
+        }
+
+        bw.write(result + "");
+        bw.flush();
+        bw.close();
+    }
+}
---
+++ baekjoon/p13549.java
@@ -0,0 +1,75 @@
+import java.io.*;
+import java.util.ArrayDeque;
+import java.util.Arrays;
+import java.util.Deque;
+import java.util.StringTokenizer;
+
+public class p13549 {
+    private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    private static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    private static StringTokenizer st;
+    private static int N, K;
+    private static int[] times;
+
+    private static void bfs(int start) {
+        Deque<Integer> deque = new ArrayDeque<>();
+        deque.add(start);
+
+        times[start] = 0;
+
+        while (!deque.isEmpty()) {
+            int curt = deque.poll();
+
+            if (curt == K) {
+                return;
+            }
+
+            // 앞 뒤로 걷기
+            int next = walk(curt, true);
+            if (next >= 0) {
+                if (times[curt] + 1 < times[next]) {
+                    times[next] = times[curt] + 1;
+                    deque.add(next);
+                }
+            }
+
+            next = walk(curt, false);
+            if (next >= 0) {
+                if (times[curt] + 1 < times[next]) {
+                    times[next] = times[curt] + 1;
+                    deque.add(next);
+                }
+            }
+
+            // 순간이동 하기
+            next = curt * 2;
+            if (next >= 0 && next < 100001) {
+                if (times[curt] < times[next]) {
+                    times[next] = times[curt];
+                    deque.addFirst(next);
+                }
+            }
+        }
+    }
+
+    private static int walk(int start, boolean isForward) {
+        int end = isForward ? start + 1 : start - 1;
+        return (end >= 0 && end < 100001) ? end : -1;
+    }
+
+    public static void main(String[] args) throws IOException {
+        st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        K = Integer.parseInt(st.nextToken());
+
+        times = new int[100001];
+        Arrays.fill(times, Integer.MAX_VALUE);
+
+        bfs(N);
+
+        bw.write(times[K] + "");
+        bw.flush();
+        bw.close();
+    }
+}
