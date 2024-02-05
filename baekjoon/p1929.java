d998d180a30dfc263e101507e9172db126d842e1
takeny1998 <89298198+takeny1998@users.noreply.github.com>
2024-02-05 13:47:56 +0900

    Solve p1463, p1929, p1987

---
+++ baekjoon/p1929.java
@@ -0,0 +1,38 @@
+import java.io.*;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class p1929 {
+    private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    private static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    private static StringTokenizer st;
+    private static int M, N;
+    private static boolean[] isPrime;
+
+    public static void main(String[] args) throws IOException {
+        st = new StringTokenizer(br.readLine());
+
+        M = Integer.parseInt(st.nextToken());
+        N = Integer.parseInt(st.nextToken());
+
+        isPrime = new boolean[N + 1];
+        Arrays.fill(isPrime, true);
+
+        isPrime[0] = false;
+        isPrime[1] = false;
+
+        for (int i = 2; i * i <= N; i ++) {
+            if (!isPrime[i]) continue;
+
+            for (int j = i * i; j <= N; j += i) {
+                isPrime[j] = false;
+            }
+        }
+
+        for (int i = M; i <= N; i ++) {
+            if (isPrime[i]) bw.write(i + "\n");
+        }
+        bw.flush();
+        bw.close();
+    }
+}
