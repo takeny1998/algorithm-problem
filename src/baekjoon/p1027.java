175b201f2130af17c2fa5e5b734722ce91049c30
Heebeom Yang <takeny1998@gmail.com>
2025-08-19 11:52:06 +0900

    soilve p1027, p16234

---
+++ src/baekjoon/p1027.java
@@ -0,0 +1,61 @@
+package baekjoon;
+
+import java.io.*;
+import java.util.*;
+
+public class p1027 {
+
+    private static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    private static final BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    private static StringTokenizer st;
+
+    private static long[] arr;
+
+    private static int N;
+
+    private static double getDelta(int i, int r) {
+        return (double) (arr[i] - arr[r]) / (i - r);
+    }
+
+    public static void main(String[] args) throws Exception {
+
+        N = Integer.parseInt(br.readLine());
+        arr = new long[N];
+
+        st = new StringTokenizer(br.readLine());
+
+        for (int i = 0; i < N; i ++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int answer = 0;
+
+        for (int curt = 0; curt < N; curt ++) {
+            int count = 0;
+
+            double leftMin = Double.MAX_VALUE, rightMax = -Double.MAX_VALUE;
+
+            for (int left = (curt - 1); left >= 0; left --) {
+                double delta = getDelta(left, curt);
+                if (delta >= leftMin) continue;
+                leftMin = delta;
+                count ++;
+            }
+
+            for (int right = curt + 1; right < N; right ++) {
+                double delta = getDelta(curt, right);
+
+                if (delta <= rightMax) continue;
+                rightMax = delta;
+                count ++;
+            }
+
+            answer = Math.max(answer, count);
+        }
+
+        bw.write(answer + "\n");
+        bw.flush();
+    }
+
+}
---
+++ src/baekjoon/p16234.java
@@ -0,0 +1,127 @@
+package baekjoon;
+
+import java.io.*;
+import java.util.*;
+
+public class p16234 {
+
+    static class Element {
+
+        private final int y, x;
+
+        public Element(int y, int x) {
+            this.y = y;
+            this.x = x;
+        }
+
+    }
+
+    private static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    private static final BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    private static StringTokenizer st;
+
+    private static int N, L, R;
+
+    private static int[][] arr;
+
+    private static final int[] dy = { -1, 0, 1, 0 }, dx = { 0, 1, 0, -1 };
+
+    public static int bfs() {
+        final int[][] visited = new int[N][N];
+        final Queue<Element> queue = new ArrayDeque<>();
+        int cnt = 1;
+
+        for (int y = 0; y < N; y ++) {
+            for (int x = 0; x < N; x ++) {
+                if (visited[y][x] != 0) continue;
+
+                queue.add(new Element(y, x));
+
+                while (!queue.isEmpty()) {
+                    final Element elm = queue.poll();
+
+                    final int curt = arr[elm.y][elm.x];
+
+                    for (int d = 0; d < 4; d ++) {
+                        final int ny = elm.y + dy[d], nx = elm.x + dx[d];
+
+                        if (ny >= N || ny < 0 || nx >= N || nx < 0) continue;
+                        if (visited[ny][nx] != 0) continue;
+
+                        final int next = arr[ny][nx];
+                        final int diff = Math.abs(curt - next);
+
+                        if (diff >= L && diff <= R) {
+                            visited[ny][nx] = cnt;
+                            queue.add(new Element(ny, nx));
+                        }
+                    }
+
+                }
+
+                cnt ++;
+            }
+        }
+
+        int changed = 0;
+
+        for (int i = 1; i <= cnt; i ++) {
+            int sum = 0;
+            int count = 0;
+
+            for (int y = 0; y < N; y ++) {
+                for (int x = 0; x < N; x ++) {
+                    if (visited[y][x] != i) continue;
+                    sum += arr[y][x];
+                    count ++;
+                }
+            }
+
+            if (count == 0) continue;
+
+            int avg = sum / count;
+
+            for (int y = 0; y < N; y ++) {
+                for (int x = 0; x < N; x ++) {
+                    if (visited[y][x] != i) continue;
+                    arr[y][x] = avg;
+                    changed ++;
+                }
+            }
+        }
+
+        return changed;
+    }
+
+    public static void main(String[] args) throws Exception {
+        st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        L = Integer.parseInt(st.nextToken());
+        R = Integer.parseInt(st.nextToken());
+
+        arr = new int[N][N];
+
+        for (int i = 0; i < N; i ++) {
+            st = new StringTokenizer(br.readLine());
+
+            for (int r = 0; r < N; r ++) {
+                arr[i][r] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        int answer = 0;
+
+        while (true) {
+            final int changed = bfs();
+            answer ++;
+
+            if (changed == 0) break;
+        }
+
+        bw.write((answer - 1) + "\n");
+        bw.flush();
+    }
+
+}
