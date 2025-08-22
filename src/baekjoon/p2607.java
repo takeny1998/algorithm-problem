450874da0ef7a58c58dba64c3034182838f2ce87
Heebeom Yang <takeny1998@gmail.com>
2025-08-22 10:40:52 +0900

    solve p2607

---
+++ src/baekjoon/p2607.java
@@ -0,0 +1,62 @@
+package baekjoon;
+
+import java.io.*;
+
+public class p2607 {
+
+    private static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    private static final BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    private static int N;
+
+    private static String word;
+
+    public static void main(String[] args) throws Exception {
+        N = Integer.parseInt(br.readLine());
+
+        word = br.readLine();
+
+        int answer = 0;
+
+        for (int i = 1; i < N; i++) {
+
+            final String otherWord = br.readLine();
+
+            int points = 0;
+            int otherPoints = 0;
+
+            boolean[] visited = new boolean[word.length()];
+            boolean[] otherVisited = new boolean[otherWord.length()];
+
+            for (int r = 0; r < word.length(); r++) {
+                for (int c = 0; c < otherWord.length(); c++) {
+                    if (otherVisited[c]) continue;
+                    if (word.charAt(r) == otherWord.charAt(c)) {
+                        otherVisited[c] = true;
+                        points++;
+                        break;
+                    }
+                }
+            }
+
+            for (int c = 0; c < otherWord.length(); c++) {
+                for (int r = 0; r < word.length(); r++) {
+                    if (visited[r]) continue;
+                    if (otherWord.charAt(c) == word.charAt(r)) {
+                        visited[r] = true;
+                        otherPoints++;
+                        break;
+                    }
+                }
+            }
+
+            if ((word.length() - points) <= 1 && (otherWord.length() - otherPoints) <= 1) {
+                answer++;
+            }
+        }
+
+        bw.write(answer + "\n");
+        bw.flush();
+    }
+
+}
