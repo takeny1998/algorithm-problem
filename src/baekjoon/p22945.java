71a828433caeeb38ed74037dcadbfc53109158f4
Heebeom Yang <takeny1998@gmail.com>
2025-08-30 15:19:01 +0900

    solve p22945

---
+++ src/baekjoon/p22945.java
@@ -0,0 +1,48 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class p22945 {
+    
+    private static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    private static final BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    private static StringTokenizer st;
+
+    private static int[] x;
+
+    private static int N;
+
+    public static void main(String[] args) throws Exception {
+        N = Integer.parseInt(br.readLine());
+        st = new StringTokenizer(br.readLine());
+
+        x = new int[N];
+
+        for (int i = 0; i < N; i ++) {
+            x[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int left = 0, right = N - 1;
+
+        int answer = 0;
+
+        while (left < right) {
+            final int score = (right - left - 1) * Math.min(x[left], x[right]);
+
+            answer = Math.max(answer, score);
+            
+            if (x[left] <= x[right]) left ++;
+            else right --;
+        }
+
+        bw.write(answer + "\n");
+        bw.flush();
+    }
+
+}
