9a1163ee7b67e4ef2462ab55cc85958efe6a289f
Heebeom Yang <takeny1998@gmail.com>
2025-10-14 11:41:09 +0900

    solve p1504, p2667

---
+++ src/baekjoon/p1504.java
@@ -0,0 +1,113 @@
+package baekjoon;
+
+import java.io.*;
+import java.util.*;
+
+public class p1504 {
+
+    private static class Node implements Comparable<Node> {
+
+        private int index, dist;
+
+        Node(int index, int dist) {
+            this.index = index;
+            this.dist = dist;
+        }
+
+        @Override
+        public int compareTo(Node node) {
+            return this.dist - node.dist;
+        }
+
+    }
+
+    private static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    private static final BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    // 실수: answer 도출 중 Integer.MAX_VALUE를 세개 더하면 오버풀로우 남
+    private static final int MAX = (800 * 1000) * 10;
+
+    private static StringTokenizer st;
+    
+    private static int N, E, a, b, c, v1, v2;
+
+    private static List<Node>[] graph;
+
+    private static int[] dijkstra(int start) {
+       
+        Queue<Node> queue = new PriorityQueue<>();
+        queue.add(new Node(start, 0));
+
+        int[] dists = new int[N + 1];
+        Arrays.fill(dists, MAX);
+        dists[start] = 0;
+
+        while (!queue.isEmpty()) {
+            Node curt = queue.poll();
+
+            if (curt.dist > dists[curt.index]) continue;
+
+            for (Node next : graph[curt.index]) {
+                int dist = curt.dist + next.dist;
+
+                if (dist >= dists[next.index]) continue;
+
+                dists[next.index] = dist;
+                queue.add(new Node(next.index, dist));
+            }
+        }
+
+        return dists;
+    }
+    
+
+    public static void main(String[] args) throws Exception {
+        st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+
+        graph = new ArrayList[N + 1];
+
+        for (int n = 0; n <= N; n ++) {
+            graph[n] = new ArrayList<>();
+        }
+
+        E = Integer.parseInt(st.nextToken());
+
+        for (int e = 0; e < E; e ++) {
+            st = new StringTokenizer(br.readLine());
+
+            a = Integer.parseInt(st.nextToken());
+            b = Integer.parseInt(st.nextToken());
+            c = Integer.parseInt(st.nextToken());
+
+            graph[a].add(new Node(b, c));
+            graph[b].add(new Node(a, c));
+        }
+
+        st = new StringTokenizer(br.readLine());
+        v1 = Integer.parseInt(st.nextToken());
+        v2 = Integer.parseInt(st.nextToken());
+
+        int[] distV1 = dijkstra(v1);
+        int[] distV2 = dijkstra(v2);
+
+        // 실수: v1과 v2 사이를 고려할 필요 없음
+        // - 만약 사이에 시작점, 다른 정점이 존재해도 다 dist 배열에 반영되어있음
+        // - 단순히 (시작 > v1 > v2 > 끝) 혹은 (시작 > v2 > v1 > 끝) 만 파악 OK
+        int answer = Math.min(
+            distV1[1] + distV1[v2] + distV2[N],
+            distV2[1] + distV2[v1] + distV1[N]
+        );
+
+        if (answer >= MAX) {
+            bw.write("-1\n");
+        } else {
+            bw.write(answer + "\n");
+        }
+
+        bw.flush();
+    }
+    
+}
---
+++ src/baekjoon/p2667.java
@@ -0,0 +1,93 @@
+package baekjoon;
+
+import java.io.*;
+import java.util.*;
+
+public class p2667 {
+
+    private static class Element {
+
+        private int y, x;
+
+        Element(int y, int x) {
+            this.y = y;
+            this.x = x;
+        }
+
+    }
+
+    private static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    private static final BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    private static final Queue<Element> queue = new ArrayDeque<>();
+
+    private static final List<Integer> answers = new ArrayList<>();
+
+    private static final int[] DY = { -1, 0, 1, 0 }, DX = { 0, 1, 0, -1 };
+
+    private static int N;
+
+    private static int[][] matrix;
+
+    private static boolean[][] visited;
+
+    private static void bfs(int sy, int sx) {
+
+        queue.add(new Element(sy, sx));
+        visited[sy][sx] = true;
+
+        int answer = 1;
+
+        while (!queue.isEmpty()) {
+            Element curt = queue.poll();
+
+            for (int d = 0; d < 4; d ++) {
+                Element next = new Element(curt.y + DY[d], curt.x + DX[d]);
+
+                if (next.y < 0 || next.y >= N || next.x < 0 || next.x >= N) continue;
+                if (visited[next.y][next.x]) continue;
+                if (matrix[next.y][next.x] == 0) continue;
+
+                visited[next.y][next.x] = true;
+                answer ++;
+                queue.add(next);
+            }
+        }
+
+        answers.add(answer);
+    }
+
+    public static void main(String[] args) throws Exception {
+        
+        N = Integer.parseInt(br.readLine());
+
+        matrix = new int[N][N];
+        visited = new boolean[N][N];
+
+        for (int y = 0; y < N; y ++) {
+            char[] chars = br.readLine().toCharArray();
+
+            for (int x = 0; x < N; x ++) {
+                matrix[y][x] = Integer.parseInt(chars[x] + "");
+            }
+        }
+
+        for (int y = 0; y < N; y ++) {
+            for (int x = 0; x < N; x ++) {
+                if (visited[y][x] || matrix[y][x] == 0) continue;
+                bfs(y, x);
+            }
+        }
+
+        Collections.sort(answers);
+
+        bw.write(answers.size() + "\n");
+
+        for (int answer : answers) {
+            bw.write(answer + "\n");
+        }
+        bw.flush();
+    }
+
+}
