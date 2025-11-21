537c6b1843417b23922e4e552d08ed9038c40c46
Heebeom Yang <takeny1998@gmail.com>
2025-11-21 14:41:55 +0900

    solve p34755

--- practice/baekjoon/java/p34755.java
+++ practice/baekjoon/java/p34755.java
@@ -1,15 +1,13 @@
-package baekjoon;
-
 import java.io.*;
 import java.util.*;
 
 public class p34755 {
-    
+
     private static class Marine implements Comparable<Marine> {
 
-        int rank, honor, serial;
+        long rank, honor, serial;
 
-        Marine(int rank, int honor, int serial) {
+        Marine(long rank, long honor, long serial) {
             this.rank = rank;
             this.honor = honor;
             this.serial = serial;
@@ -17,22 +15,14 @@ public class p34755 {
 
         @Override
         public int compareTo(Marine marine) {
-            if (marine == null) return 1;
-
             if (rank == marine.rank) {
                 if (honor == marine.honor) {
-                    return Integer.compare(marine.serial, serial);
+                    return Long.compare(marine.serial, serial);
                 }
-                return Integer.compare(honor, marine.honor);
+                return Long.compare(honor, marine.honor);
             }
-            return Integer.compare(rank, marine.rank);
+            return Long.compare(rank, marine.rank);
         }
-
-        @Override
-        public String toString() {
-            return "(rank=%d, honor=%d, serial=%d)".formatted(rank, honor, serial);
-        }
-
     }
 
     private static final BufferedReader br = new BufferedReader(
@@ -43,119 +33,74 @@ public class p34755 {
         new OutputStreamWriter(System.out)
     );
 
-    private static StringTokenizer st;
+    private static StringTokenizer st1, st2;
 
     private static int N, Q;
 
-    private static int[] ranks, honors;
+    private static TreeSet<Marine> marines = new TreeSet<>();
 
-    private static List<Marine> marines;
+    private static TreeMap<Long, Marine> serialMap = new TreeMap<>();
 
-    private static Map<Integer, Integer> marineIndex;
-
-    private static int upperBound(int target) {
-        
-        int low = 1, high = N;
-
-        while (low < high) {
-            int mid = (low + high) / 2;
-            if (mid <= target) low = mid + 1;
-            else high = mid;
-        }
-
-        return high;
-    }
-
-    private static int lowerBound(Marine target) {
-
-        int low = 1, high = N;
-
-        while (low < high) {
-            int mid = (low + high) / 2;
-
-            if (marines.get(mid).compareTo(target) < 0) low = mid + 1;
-            else high = mid;
-        }
-
-        return high;
+    private static Marine findSenior(Marine target) {
+        Marine key = new Marine(target.rank, target.honor, 0);
+        return marines.higher(key);
     }
 
     public static void main(String[] args) throws Exception {
-        
+
         N = Integer.parseInt(br.readLine());
 
-        ranks = new int[N + 1];
-        st = new StringTokenizer(br.readLine());
-        for (int i = 1; i <= N; i ++) {
-            ranks[i] = Integer.parseInt(st.nextToken());
-        }
+        st1 = new StringTokenizer(br.readLine());
+        st2 = new StringTokenizer(br.readLine());
 
-        honors = new int[N + 1];
-        st = new StringTokenizer(br.readLine());
-        for (int i = 1; i <= N; i ++) {
-            honors[i] = Integer.parseInt(st.nextToken());
-        }
-        
-        marineIndex = new HashMap<>();
-        marines = new ArrayList<>();
-        marines.add(null);
+        for (int i = 0; i < N; i ++) {
+            long rank = Long.parseLong(st1.nextToken());
+            long honor = Long.parseLong(st2.nextToken());
+            long serial = i + 1;
 
-        for (int i = 1; i <= N; i ++) {
-            Marine marine = new Marine(ranks[i], honors[i], i);
+            Marine marine = new Marine(rank, honor, serial);
             marines.add(marine);
-        }
-
-        Collections.sort(marines);
-
-        for (int i = 1; i <= N; i ++) {
-            marineIndex.put(marines.get(i).serial, i);
+            serialMap.put(serial, marine);
         }
 
         Q = Integer.parseInt(br.readLine());
 
         for (int q = 0; q < Q; q ++) {
+            st1 = new StringTokenizer(br.readLine());
 
-            st = new StringTokenizer(br.readLine());
-            
-            int command = Integer.parseInt(st.nextToken());
-            int to = Integer.parseInt(st.nextToken());
+            int comm = Integer.parseInt(st1.nextToken());
 
-            Marine marine = marines.get(marineIndex.get(to));
-            Marine senior = null;
-            
-            if (command == 1) {
-                marine.rank = Integer.parseInt(st.nextToken());
-                marine.honor = Integer.parseInt(st.nextToken());
+            Marine marine = serialMap.get(Long.parseLong(st1.nextToken()));
+            marines.remove(marine);
+
+            if (comm == 1) {
+                marine.rank = Long.parseLong(st1.nextToken());
+                marine.honor = Long.parseLong(st1.nextToken());
 
             } else {
-                senior = marines.get(upperBound(to));
-                int diff = marine.honor / 2;
+                Marine senior = findSenior(marine);
 
-                if (marine.serial == senior.serial || (marine.rank == senior.rank && marine.honor == senior.honor)) {
-                    marine.honor -= diff;
-                    bw.write(marine.rank + " " + marine.honor + " -1 -1\n");
-                } else {
-                    marine.honor -= diff;
-                    senior.honor += diff;
-                    bw.write(marine.rank + " " + marine.honor + " " + senior.rank + " " + senior.honor + "\n");
+                long seniorRank = -1;
+                long seniorHonor = -1;
+
+                if (senior != null) {
+                    marines.remove(senior);
+
+                    senior.honor += marine.honor / 2;
+                    seniorRank = senior.rank;
+                    seniorHonor = senior.honor;
+
+                    marines.add(senior);
                 }
-            }
-            
-            int i = marineIndex.get(marine.serial);
-            marines.remove(i);
-            int index = lowerBound(marine);
-            marines.add(index, marine);
-            marineIndex.put(marine.serial, Math.min(N, index + 1));
 
-            if (senior != null) {
-                int si = marineIndex.get(senior.serial);
-                marines.remove(si);
-
-                int seniorIdx = lowerBound(senior);
-                marines.add(seniorIdx, senior);
-                marineIndex.put(senior.serial, Math.min(N, seniorIdx + 1));
+                marine.honor -= marine.honor / 2;
+                bw.write(marine.rank + " " + marine.honor + " " + seniorRank + " " + seniorHonor + "\n");
             }
+
+            marines.add(marine);
         }
+
         bw.flush();
+
     }
 }
